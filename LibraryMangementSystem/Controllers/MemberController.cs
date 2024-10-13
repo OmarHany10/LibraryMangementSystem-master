f65a2a5eac23572dd49d690d3a266614ccda1cfc
OmarHany10 <121138912+OmarHany10@users.noreply.github.com>
2024-10-13 18:34:16 +0300

    Member portal

--- LibraryMangementSystem/Controllers/MemberController.cs
+++ LibraryMangementSystem/Controllers/MemberController.cs
@@ -1,12 +1,14 @@
 ﻿using LibraryMangementSystem.Models;
 using LibraryMangementSystem.Repository;
 using LibraryMangementSystem.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 
 namespace LibraryMangementSystem.Controllers
 {
+    [Authorize(Roles = "Members")]
     public class MemberController : Controller
     {
         private readonly UserManager<ApplicationUser> userManager;
@@ -56,5 +58,49 @@ namespace LibraryMangementSystem.Controllers
             }
             return View("BorrowingHistory", borrowingHistories);
         }
+
+        public IActionResult MyBorrowedBook()
+        {
+            string email = User.Identity.Name;
+            Member member = memberRepository.GetByEmail(email);
+            List<Checkout> checkouts = checkoutRepository.GetByMemberID(member.MemberId);
+            List<BorrowingHistoryViewModel> borrowingHistories = new List<BorrowingHistoryViewModel>();
+            foreach (var checkout in checkouts)
+            {
+                BorrowingHistoryViewModel borrowingHistory = new BorrowingHistoryViewModel();
+                if (checkout?.ReturnDate == null)
+                {
+                    borrowingHistory.BookName = checkout.Book.Title;
+                    borrowingHistory.CheckoutDate = checkout.CheckoutDate;
+                    borrowingHistory.DueDate = checkout.DueDate;
+                    borrowingHistory.ReturnDate = checkout.ReturnDate;
+
+                    // Create a new Penalty object if it doesn't exist
+                    Penalty penalty = new Penalty
+                    {
+                        LateDaysThreshold = 0, // Set your desired threshold here (in days)
+                        PenaltyAmount = 2, // Default penalty per day (for example, $2 per day)
+                        CheckoutId = checkout.CheckoutId
+                    };
+                    var lateMinutes = (DateTime.Now - checkout.DueDate).TotalMinutes;
+                    // Apply penalty based on minutes
+                    if (lateMinutes > penalty.LateDaysThreshold * 1440) // Threshold converted to minutes
+                    {
+                        var minutesWithPenalty = (decimal)(lateMinutes - (penalty.LateDaysThreshold * 1440)); // Threshold converted to minutes
+                        penalty.PenaltyAmount = minutesWithPenalty * (penalty.PenaltyAmount); // Penalty per minute
+                    }
+                    else
+                    {
+                        penalty.PenaltyAmount = 0; // No penalty if within the threshold
+                    }
+                    borrowingHistory.PenalityAmount = penalty.PenaltyAmount;
+                    borrowingHistories.Add(borrowingHistory);
+                }
+            }
+            return View("MyBorrowedBook", borrowingHistories);
+        }
+
+        
+
     }
 }
